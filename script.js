908787c84175cfcec9130a1964a57b4f1b9f6bd0
frabinstein <frabinstein@users.noreply.github.com>
2019-08-18 14:43:18 -0300

    Mejoras

--- script.js
+++ script.js
@@ -27,13 +27,16 @@
   }
 
   async function llenarTabla(site_id, seller_id) {
-    let items = await buscarItemsPorSiteYSeller(site_id, seller_id).then(json => json.results.map(function(x) {return {seller_id: x.seller.id, item_id: x.id, item_title: x.title, category_id: x.category_id}; }));
+    let items = await buscarItemsPorSiteYSeller(site_id, seller_id)
+    .then(json => json.results.map(x => {return {seller_id: x.seller.id, item_id: x.id, item_title: x.title, category_id: x.category_id}; }));
 
 	for(let i=0; i<items.length; i++) {
-		items[i].category_name = await obtenerDetalleDeCategoria(items[i].category_id).then(json => json.name);
+		items[i].category_name = await obtenerDetalleDeCategoria(items[i].category_id)
+        .then(json => json.name);
 	}
 
-	app.resultados = app.resultados.concat(items);
+    console.log("items = " + JSON.stringify(items));
+    return items;
   }
 
 
@@ -53,14 +56,30 @@
       sitios: []
     },
     methods: {
-      onSubmit() {
+      async onSubmit() {
         app.inicializado = true;
         app.site_id = this.site_id;
-        app.seller_id = this.seller_id.split(",");
-        app.seller_id.forEach(function(x, i) {
+        if(typeof app.seller_id === "string") {
+          app.seller_id = this.seller_id.split(",");
+        }
+        let resultadosParciales = app.seller_id.map(async(x, i) => {
           app.seller_id[i] = app.seller_id[i].trim();
-          llenarTabla(app.site_id, app.seller_id[i]);
+          console.log("corrida " + i);
+          return await llenarTabla(app.site_id, app.seller_id[i]);
         });
+/*
+        resultadosParciales = app.seller_id.reduce(async(v, x, i) => {
+            app.seller_id[i] = app.seller_id[i].trim();
+            console.log("corrida " + i);
+            return v.concat(await llenarTabla(app.site_id, app.seller_id[i]));
+          }, []);
+        console.log("resultadosParciales = " + JSON.stringify(await Promise.all(resultadosParciales)));
+        console.log("resultadosParciales reducidos = " + JSON.stringify(await Promise.all(resultadosParciales.reduce(function(v, x) { return v.concat(x); }, []))));
+        app.resultados = await Promise.all(resultadosParciales.reduce(function(v, x) { return v.concat(x); }, []));
+*/
+        app.resultados = await Promise.all(resultadosParciales);
+        app.resultados = app.resultados.reduce(function(v, x) { return v.concat(x); }, []);
+        console.log("finalizado");
         app.finalizado = true;
       },
       onReset() {
@@ -75,7 +94,7 @@
         const e = document.createEvent('MouseEvents'),
         a = document.createElement('a');
         a.download = "";
-        app.seller_id.forEach(function(x) { a.download = a.download + x + "-" });
+        app.seller_id.forEach(x => { a.download = a.download + x + "-" });
         a.download = a.download + app.site_id + ".json";
         a.href = window.URL.createObjectURL(blob);
         a.click();
